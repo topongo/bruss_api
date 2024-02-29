34c00287c2fa0a3af291d5e9e2eb2256e1101f7d
topongo <lorenzo.bodini.private@gmail.com>
2024-02-29 11:40:03 +0100

    moved to mongodb

--- src/routes/map.rs
+++ src/routes/map.rs
@@ -1,9 +1,10 @@
+use rocket::futures::StreamExt;
 use rocket::serde::json::Json;
 use crate::data::{Area, AreaType};
-use tokio_postgres::NoTls;
-use tokio_postgres::row::Row;
-use crate::configs::BrussConfig;
+use crate::configs::CONFIGS;
 use rocket::catcher::Result;
+use mongodb::{bson::doc, Client};
+use futures::stream::TryStreamExt;
 
 fn db_areas() -> Vec<Area> {
    vec![] 
@@ -11,17 +12,14 @@ fn db_areas() -> Vec<Area> {
 
 #[get("/areas")]
 pub async fn get_areas() -> Json<Vec<Area>> {
-    let config = BrussConfig::from_file("/home/topongo/fast/documents/uni/internship/bruss/app/api/config.toml").get_db_configs();
-    let (client, connection) = config.connect(NoTls).await.unwrap();
-    tokio::spawn(async move {
-        if let Err(e) = connection.await {
-            eprintln!("connection error: {}", e);
-        }
-    });
+    let db_config = CONFIGS.db.gen_mongodb_options();
+    let client = Client::with_options(db_config).unwrap();
 
-    let rows: Vec<tokio_postgres::row::Row> = client.query("SELECT id, ty, label FROM areas", &[]).await.unwrap();
+    let db = client.database(CONFIGS.db.get_db());
 
-    let areas: Vec<Area> = serde_postgres::from_rows(&rows).unwrap();
-    Json(areas)
+    let areas_c = db.collection::<Area>("areas");
+    let cursor = areas_c.find(doc! {}, None).await.unwrap();
+
+    Json(cursor.try_collect::<Vec<Area>>().await.unwrap())
 }
 
