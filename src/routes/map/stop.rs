5c69263dee66ec1c290c28e2f1e5485da6027184
topongo <lorenzo.bodini.private@gmail.com>
2024-06-09 17:24:14 +0200

    add routes for time-aware queries

--- src/routes/map/stop.rs
+++ src/routes/map/stop.rs
@@ -1,8 +1,9 @@
-use bruss_data::{Stop, Trip};
+use bruss_config::CONFIGS;
+use bruss_data::{BrussType, Route, Stop, Trip};
 use lazy_static::lazy_static;
 use crate::db::BrussData;
 use super::{gen_area_getters, query::{DBInterface, DBQuery, Queriable}, params::{Id, ParamQuery}, AreaTypeWrapper, trip::TripQuery};
-use mongodb::bson::Document;
+use mongodb::bson::{doc, Document};
 use rocket_db_pools::Connection;
 use crate::response::ApiResponse;
 use rocket::{request::FromParam,form::Strict};
@@ -39,7 +40,25 @@ async fn get_trips(
     Queriable::<Vec<Trip>>::query(&DBInterface(db), query?.into_inner().to_doc_stop(id as u16, area_type?.value().inner)).await.into()
 }
 
+#[get("/<area_type>/<id>/routes")]
+async fn get_routes(
+    db: Connection<BrussData>,
+    area_type: Result<Id<AreaTypeWrapper>, <Id<AreaTypeWrapper> as FromParam<'_>>::Error>, 
+    id: Result<Id<u16>, <Id<u16> as FromParam<'_>>::Error>
+) -> ApiResponse<Vec<Route>> {
+    let id = id?.value();
+    let ty: &str = area_type?.value().into();
+    
+    let route_ids = db
+        .database(CONFIGS.db.get_db())
+        .collection::<i32>("trips")
+        .distinct("route", doc!{ "type": ty, "$or": [ { format!("times.{}", id): { "$exists": true } }, { format!("times.{}", id): { "$exists": true } } ] }, None)
+        .await?;
+        
+    Queriable::<Vec<Route>>::query(&DBInterface(db), doc!{"id": {"$in": route_ids}}).await.into()
+}
+
 lazy_static!{
-    pub static ref ROUTES: Vec<rocket::Route> = routes![get, get_opts, get_trips];
+    pub static ref ROUTES: Vec<rocket::Route> = routes![get, get_opts, get_trips, get_routes];
 }
 
--- src/routes/map/trip.rs
+++ src/routes/map/trip.rs
@@ -1,3 +1,4 @@
+use chrono::{NaiveTime, Timelike};
 use rocket::form::FromForm;
 use tt::AreaType;
 use mongodb::bson::{doc, Document};
@@ -5,24 +6,70 @@ use mongodb::bson::{doc, Document};
 #[derive(FromForm)]
 pub struct TripQuery {
     id: Option<String>,
+    time: Option<String>,
 }
 
 impl TripQuery {
+    fn to_doc_time_stop(time: String, stop: u16) -> Document {
+        let t = NaiveTime::parse_from_str(&time, "%H:%M").unwrap();
+        doc!{
+            "$expr": {
+                "$and": [
+                    {"$gte": [{"$hour": {"$toDate": { "$concat": ["1970-01-01T", format!("$times.{}.departure", stop), "Z"]}}}, t.hour() as i32]},
+                    {"$gte": [{"$minute": {"$toDate": { "$concat": ["1970-01-01T", format!("$times.{}.departure", stop), "Z"]}}}, t.minute() as i32]},
+                ]
+            }
+        }
+    }
+
+    fn to_doc_time_route(time: String) -> Document {
+        let t = NaiveTime::parse_from_str(&time, "%H:%M").unwrap();
+        doc!{
+            "$expr": {
+                "$anyElementTrue": {
+                    "$map": {
+                        "input": { "$objectToArray": "$times" },
+                        "in": {
+                            "$gt": [{ "$hour": { "$toDate": { "$concat": ["1970-01-01T", "$$this.v.departure", "Z"] } } }, t.hour() as i32],
+                            "$gt": [{ "$minute": { "$toDate": { "$concat": ["1970-01-01T", "$$this.v.departure", "Z"] } } }, t.minute() as i32],
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     pub fn to_doc_stop(self, stop: u16, ty: AreaType) -> Document {
-        let Self { id } = self;
-        let mut d = Document::new();
-        d.insert(format!("times.{}", stop), doc!{"$exists": true});
-        d.insert::<&'static str, &'static str>("type", ty.into());
-        if let Some(id) = id { d.insert("id", id.clone()); }
+        let Self { id, time } = self;
+        let ty_st: &str = ty.into();
+        let mut conds = vec![doc!{"type": ty_st}, doc!{format!("times.{}", stop): doc!{"$exists": true}}];
+        if let Some(time) = time {
+            conds.push(Self::to_doc_time_stop(time, stop));
+        }
+        if let Some(id) = id { conds.push(doc!{"id": id.clone()}) }
+        let d = doc!{"$and": conds};
+        info!("{:?}", d);
         d
     }
 
     pub fn to_doc_route(self, route: u16) -> Document {
-        let Self { id } = self;
-        let mut d = Document::new();
-        d.insert("route", route as i32);
-        if let Some(id) = id { d.insert("id", id.clone()); }
-        d 
+        let Self { id, time } = self;
+        let mut conds = vec![doc!{"route": route as i32}];
+        if let Some(time) = time {
+            conds.push(Self::to_doc_time_route(time));
+        }
+        if let Some(id) = id { conds.push(doc!{"id": id.clone()}) }
+        let d = doc!{"$and": conds};
+        info!("{:?}", d);
+        d
     }
+    
+    // pub fn to_doc_route(self, route: u16) -> Document {
+    //     let Self { id } = self;
+    //     let mut d = Document::new();
+    //     d.insert("route", route as i32);
+    //     if let Some(id) = id { d.insert("id", id.clone()); }
+    //     d 
+    // }
 }
 
