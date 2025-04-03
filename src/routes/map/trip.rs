fe809ba6c46120cb3d0083de22af40049f1b0bdb
Lorenzo Bodini <lorenzo.bodini@prabo.org>
2025-04-03 17:19:57 +0200

    feat: trip query over route

--- src/routes/map/trip.rs
+++ src/routes/map/trip.rs
@@ -112,6 +112,45 @@ impl MultiTripQuery {
     }
 
     pub fn into_pipeline_stop(self, stop: u16, area_type: AreaType, skip: Option<u32>, limit: Option<u32>) -> CustomPipeline {
+        let Self { time, direction } = self;
+
+        let time = match time {
+            Some(t) => rocket_time_to_chrono_utc(t),
+            None => Utc::now(),
+        };
+
+        let stop_time_string = format!("$hints.times.{}.departure", stop);
+        let slen = stop_time_string.len();
+
+        let mut conds = vec![
+            doc!{"hints.type": area_type.to_string()},
+            doc!{&stop_time_string[1..slen]: {"$exists": true}},
+        ];
+        if let Some(direction) = direction {
+            conds.push(doc!{"hints.direction": direction.into_bson()});
+        }
+
+        let skip = skip.map(|v| v as i64).unwrap_or(0);
+        let limit = limit.map(|v| v as i64).unwrap_or_else(Pipeline::default_limit);
+
+        // we first filter out the great majority of trips using static conditions
+        let match_stage = doc!{"$match": {"$and": conds}};
+        let heuristic_match_stage = doc!{"$match": {"$and": [{"departure": {"$gte": time - TimeDelta::hours(4)}}, {"departure": {"$lt": time + TimeDelta::days(1)}}]}};
+        // we then calculate the arrival time at the stop
+        let set_stage = doc!{"$addFields": {"arrival_at_stop": {"$add": ["$departure", {"$multiply": [1000, {"$arrayElemAt": [&stop_time_string, 0]}]}]}}};
+        // then we match based on the arrival time at the stop
+        // we use a 20 minutes buffer to account for delays
+        let match_arrival_stage = doc!{"$match": {"arrival_at_stop": {"$gte": time - TimeDelta::minutes(20)}}};
+        // we sort by the arrival time at the stop
+        let sort_stage = doc!{"$sort": {"arrival_at_stop": 1}};
+        let lookup_stage = doc!{"$lookup": {"from": "trips","localField": "id","foreignField": "id","as": "trip"}};
+        let unwind_stage = doc!{"$unwind": "$trip"};
+        let skip_stage = doc!{"$skip": skip};
+        let limit_stage = doc!{"$limit": limit};
+        let project_stage = doc!{"$project": {"_id": 0,"trip": 1,"departure": 1, "arrival_at_stop": 1}};
+        let count_stage = doc!{"$count": "count"};
+
+
         // vec![
         //     doc!{"$match": {"$and": [
         //         // filter by area
@@ -135,9 +174,29 @@ impl MultiTripQuery {
         //     doc!{"$project": {"_id": 0,"trip": 1,"departure": 1}},
         //     // doc!{"$set": {"arrival_at_stop": }}
         // ]
-        let Self { time, .. } = self;
+        
+        let count = vec![
+            match_stage.clone(),
+            heuristic_match_stage.clone(),
+            set_stage.clone(),
+            match_arrival_stage.clone(),
+            count_stage,
+        ];
 
-        todo!()
+        let fetch = vec![
+            match_stage,
+            heuristic_match_stage,
+            set_stage,
+            match_arrival_stage,
+            sort_stage,
+            lookup_stage,
+            unwind_stage,
+            skip_stage,
+            limit_stage,
+            project_stage
+        ];
+
+        Pipeline::custom(fetch, count)
     }
     
     // pub fn to_doc_route(self, route: u16) -> Document {
